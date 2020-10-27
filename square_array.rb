746559263a37495577791ca94396559210aee403
Delia Constantino <dconstan92@gmail.com>
2020-10-27 19:45:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-# def square_array(array)
-# squared_array = Array.new
-#   array.each do |num|
-#     num_squared = num ** 2
-#     squared_array << num_squared
-#   end
-#   squared_array
-# end
+def square_array(array)
+squared_array = Array.new
+  array.each do |num|
+    num_squared = num ** 2
+    squared_array << num_squared
+  end
+  squared_array
+end
 
 def square_array(array)
   array.collect{|num| num ** 2}
